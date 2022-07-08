893dc7a7ea9978f22d173e9721d781458763bcd9
abhinavchaudhary10 <abhinavchaudhary10@gmail.com>
2022-07-08 13:26:54 +0530

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-(skv#8i!+%if7g$w4$%(zse-px3pv&z$n#reu(^@b93bu9fng6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
